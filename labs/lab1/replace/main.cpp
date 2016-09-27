867a0d9ba0b5375af2a07e111479a2ce5573968c
A-tG <sappykda@gmail.com>
2016-09-27 17:23:27 +0300

    Код разбит на функции

--- labs/lab1/replace/main.cpp
+++ labs/lab1/replace/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void ReplaceSubstrInStr(string & str, const string & strToSearch, const string & strToReplace)
 {
 	size_t foundAt = 0;
-	string resultStr = "";
+	string resultStr;
 	while (foundAt != string::npos)
 	{
 		size_t startPos = foundAt;
@@ -26,23 +26,49 @@ void ReplaceSubstrInStr(string & str, const string & strToSearch, const string &
 	str = resultStr;
 }
 
-bool ReplaceStrInFile(ifstream & input, ofstream & output, const string & strToSearch, const string & strToReplace)
+bool CopyFileLinesStringsReplacing(ifstream & input, ofstream & output, const string & strToSearch, const string & strToReplace)
 {
 	string str;
 	while (getline(input, str))
 	{
 		ReplaceSubstrInStr(str, strToSearch, strToReplace);
-		str.append("\n");
-		if (!(output << str))
+		if (!(output << str << endl))
 		{
 			cout << "Failed to save data on disk\n";
 			return false;
 		}
 	}
+	if (!output.flush())
+	{
+		cout << "Failed to save data on disk\n";
+		return false;
+	}
 	return true;
 }
 
-int main(int argc, char * argv[])
+bool CopyFileStringReplacing(char* inputFile, char* outputFile, const string & strToSearch, const string & strToReplace)
+{
+	ifstream input(inputFile);
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << inputFile << " for reading\n";
+		return false;
+	}
+	ofstream output(outputFile);
+	if (!output.is_open())
+	{
+		cout << "Failed to open " << outputFile << " for writing\n";
+		return false;
+	}
+	if (!CopyFileLinesStringsReplacing(input, output, strToSearch, strToReplace))
+	{
+		cout << "Failed to copy file " << inputFile << "to " << outputFile << endl;
+		return false;
+	}
+	return true;
+}
+
+int main(int argc, char* argv[])
 {
 	if (argc != 5)
 	{
@@ -50,18 +76,8 @@ int main(int argc, char * argv[])
 			<< "Usage: replace.exe <input file> <output file> <search string> <replace string>\n";
 		return 1;
 	}
-	ifstream input(argv[1]);
-	if (!input.is_open())
-	{
-		cout << "Failed to open " << argv[1] << " for reading\n";
-		return 1;
-	}
-	ofstream output(argv[2]);
-	if (!output.is_open())
-	{
-		cout << "Failed to open " << argv[2] << " for writing\n";
-		return 1;
-	}
+	char* inputeFileName = argv[1];
+	char* outputFileName = argv[2];
 	string strToSearch = argv[3];
 	if (strToSearch.empty())
 	{
@@ -69,14 +85,10 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 	string strToReplace = argv[4];
-	if (!ReplaceStrInFile(input, output, strToSearch, strToReplace))
+	if (!CopyFileStringReplacing(inputeFileName, outputFileName, strToSearch, strToReplace))
 	{
 		cout << "Failed to replace string in file\n";
 		return 1;
-	}
-	if (!output.flush())
-	{
-		cout << "Failed to save data on disk";
 	}
 	return 0;
 }
